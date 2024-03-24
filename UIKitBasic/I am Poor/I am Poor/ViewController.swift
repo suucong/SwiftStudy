113e1a8f49a0c82da6086a9af5f45341d3e85d4c
suucong <sophia021210@naver.com>
2024-03-24 23:53:34 +0900

    add `UIKit Basic Section 3, 4` Collections, Constants & Variable

---
+++ UIKitBasic/I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by 노수진 on 3/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
